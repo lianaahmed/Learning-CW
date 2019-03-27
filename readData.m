e011b9cfc68d95d81428d377927fd3cc6428d4ad
Liana <lianafaahmed@gmail.com>
2019-03-27 17:55:56 +0000

    death

--- readData.m
+++ readData.m
@@ -1,3 +1,4 @@
-dset_dir = '/afs/inf.ed.ac.uk/group/teaching/inf2b/cwk2/d/s1703935/';
+%dset_dir = '/afs/inf.ed.ac.uk/group/teaching/inf2b/cwk2/d/s1703935/';
+dset_dir = 'C:\Users\Liana\Desktop\Uni\Second Year\Semester 2\INF2B\Assignment 2';
 [Xtrn, Ytrn, Xtst, Ytst] = load_my_data_set(dset_dir);
 Xtrn = double(Xtrn)/255.0;
--- task1_5.m
+++ task1_5.m
@@ -4,5 +4,9 @@ function task1_5(X, Ks)
 % Input:
 %  X  : M-by-D data matrix (double)
 %  Ks : 1-by-L vector (integer) of the numbers of nearest neighbours
-
+   
+    for k = 1:length(Ks)
+       initialCentres = find(X, k, 'first'); 
+       my_kMeansClustering(X, k, initialCentres); 
+    end
 end
